86fd811295df19ecfc8d505767e9d84086e5e550
Ravindu Lakshan Weerasinghe <ravinduarduino1000@gmail.com>
2025-02-27 16:47:52 +0530

    update

--- program.c
+++ program.c
@@ -3,9 +3,6 @@
 #include <string.h>
 #include <zip.h>
 
-#define INPUT_FILE "input.txt" // File containing possible passwords
-#define ZIP_FILENAME "example.zip"
-
 // Function to attempt extraction with a given password
 int try_extract(zip_t *zip, const char *password) {
     if (zip_set_default_password(zip, password) != 0) {
@@ -15,69 +12,57 @@ int try_extract(zip_t *zip, const char *password) {
     zip_int64_t num_entries = zip_get_num_entries(zip, 0);
     for (zip_int64_t i = 0; i < num_entries; i++) {
         struct zip_stat st;
-        zip_stat_init(&st);
         if (zip_stat_index(zip, i, 0, &st) != 0) {
             continue;
         }
-
-        zip_file_t *zf = zip_fopen_index(zip, i, 0);
-        if (!zf) {
-            continue;
+        
+        zip_file_t *file = zip_fopen_index(zip, i, 0);
+        if (file) {
+            char buffer[10];
+            if (zip_fread(file, buffer, sizeof(buffer)) > 0) {
+                zip_fclose(file);
+                return 1; // Successful extraction
+            }
+            zip_fclose(file);
         }
-
-        char *contents = malloc(st.size);
-        if (!contents) {
-            zip_fclose(zf);
-            continue;
-        }
-
-        if (zip_fread(zf, contents, st.size) < 0) {
-            free(contents);
-            zip_fclose(zf);
-            continue;
-        }
-        zip_fclose(zf);
-
-        FILE *out = fopen(st.name, "wb");
-        if (!out) {
-            free(contents);
-            continue;
-        }
-        fwrite(contents, 1, st.size, out);
-        fclose(out);
-        free(contents);
-
-        printf("Extracted: %s\n", st.name);
     }
-    return 1; // Success
+    return 0;
 }
 
 int main() {
-    FILE *pw_file = fopen(INPUT_FILE, "r");
-    if (!pw_file) {
-        printf("Failed to open input file.\n");
+    char zip_filename[256], input_filename[256];
+    
+    printf("Enter ZIP filename: eg:Hello.zip\n");
+    scanf("%255s", zip_filename);
+    
+    printf("Enter input filename: eg:Passwords.txt\n");
+    scanf("%255s", input_filename);
+    
+    FILE *fp = fopen(input_filename, "r");
+    if (!fp) {
+        perror("Failed to open input file");
         return 1;
     }
-
-    int err;
-    zip_t *zip = zip_open(ZIP_FILENAME, 0, &err);
+    
+    zip_t *zip = zip_open(zip_filename, 0, NULL);
     if (!zip) {
-        printf("Failed to open ZIP file.\n");
+        perror("Failed to open ZIP file");
+        fclose(fp);
         return 1;
     }
-
+    
     char password[256];
-    while (fgets(password, sizeof(password), pw_file)) {
-        password[strcspn(password, "\n")] = 0; // Remove newline
-        printf("Trying password: %s\n", password);
+    while (fgets(password, sizeof(password), fp)) {
+        // Remove newline character
+        password[strcspn(password, "\r\n")] = 0;
         
         if (try_extract(zip, password)) {
-            printf("Success! Password found: %s\n", password);
+            printf("Password found: %s\n", password);
             break;
         }
     }
-
-    fclose(pw_file);
+    
     zip_close(zip);
+    fclose(fp);
     return 0;
 }
